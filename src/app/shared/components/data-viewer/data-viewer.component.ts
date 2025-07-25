ca0ca4a5f9631ff6e0e3e40c0d7606d853d5f00b
MariaYanMir <maria.yanguasm@gmail.com>
2025-07-25 12:57:30 +0200

    feat: add dataViewer component

---
+++ src/app/shared/components/data-viewer/data-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-viewer.component.html',
+  styleUrl: './data-viewer.component.scss'
+})
+export class DataViewerComponent {
+
+}
